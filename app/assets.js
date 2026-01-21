d7fc157aafb58e9621066c38288b67facc184131
JoSync <info@joobsa.com>
2026-01-21 15:18:55 +0100

    Create assets.js

---
+++ app/assets.js
@@ -0,0 +1,2 @@
+// app/assets.js
+export const BACKGROUND_SVG = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cpath fill-rule='evenodd' d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.89v12.72l11 6.35 11-6.35V17.89l-11-6.35-11 6.35z' fill='%234338ca' /%3E%3C/svg%3E")`;
